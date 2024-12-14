5fa491c0ccd690277fc743e4bd9d9e86ff6f0cc5
rtrtn <rtro1407@gmail.com>
2024-12-14 17:10:00 +0900

    sql.NullTimeに変更

--- go/models.go
+++ go/models.go
@@ -6,18 +6,18 @@ import (
 )
 
 type Chair struct {
-	ID                     string    `db:"id"`
-	OwnerID                string    `db:"owner_id"`
-	Name                   string    `db:"name"`
-	Model                  string    `db:"model"`
-	IsActive               bool      `db:"is_active"`
-	AccessToken            string    `db:"access_token"`
-	CreatedAt              time.Time `db:"created_at"`
-	UpdatedAt              time.Time `db:"updated_at"`
-	Latitude               *int      `db:"latitude"`
-	Longitude              *int      `db:"longitude"`
-	TotalDistance          int       `db:"total_distance"`
-	TotalDistanceUpdatedAt time.Time `db:"total_distance_updated_at"`
+	ID                     string       `db:"id"`
+	OwnerID                string       `db:"owner_id"`
+	Name                   string       `db:"name"`
+	Model                  string       `db:"model"`
+	IsActive               bool         `db:"is_active"`
+	AccessToken            string       `db:"access_token"`
+	CreatedAt              time.Time    `db:"created_at"`
+	UpdatedAt              time.Time    `db:"updated_at"`
+	Latitude               *int         `db:"latitude"`
+	Longitude              *int         `db:"longitude"`
+	TotalDistance          int          `db:"total_distance"`
+	TotalDistanceUpdatedAt sql.NullTime `db:"total_distance_updated_at"`
 }
 
 type ChairModel struct {
